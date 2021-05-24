9cd7f3f70a7775ca08d144c2cad33b92e629f46f
Piotr <s95576@pollub.edu.pl>
2021-05-25 01:06:53 +0200

    Revision 2

--- hello.c
+++ hello.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main(){
-printf("Hello world!");
+printHello();
 return 0;
 }
---
+++ print.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "print.h"
+
+void printHello(){
+   printf("Hello world!");
+}
